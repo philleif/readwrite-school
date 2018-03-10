0ce04f578aa37885aaa0c574fdbbfcd1b75fd879
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-03-10 15:11:13 -0500

    pull images from unsplash

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 "use strict"
 
 const data = require("../lib/data")
+const images = require("../lib/images")
 const express = require("express")
+
 const router = express.Router()
 
 
@@ -10,6 +12,11 @@ router.get("/", async (req, res, next) => {
   let text = await data.fetchArticles()
 
   text = await data.catalogText(text)
+
+  for (let node of text) {
+    node.image = await images.search(node.word)
+    console.log(node.image)
+  }
 
   console.log(text)
 
