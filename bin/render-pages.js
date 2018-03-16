308854c58a635bf130ee06f0ac740ff4f2ba0ccc
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-03-16 07:23:34 -0400

    style/font updates

--- bin/render-pages.js
+++ bin/render-pages.js
@@ -2,22 +2,31 @@
 
 const { exec } = require("child_process")
 const db = require("../lib/db")
-const fs = require('fs')
-const pdf = require('html-pdf')
+const fs = require("fs")
+const pdf = require("html-pdf")
 
 const run = async () => {
   try {
-    let words = await db.Word.find({})
-    let options = { format: 'Letter' }
+    let words = await db.Word.find({}).sort("word")
+    let options = {
+      format: "Letter",
+      paginationOffset: 1,
+      renderDelay: 1000,
+      footer: {
+        height: "28mm"
+      }
+    }
 
-    for(let w in words) {
+    for (let w in words) {
       let path = await downloadAsync(words[w].word, w)
-      let html = fs.readFileSync(path, 'utf8')
+      let html = fs.readFileSync(path, "utf8")
 
-      pdf.create(html, options).toFile(`./tmp/pdf/${w}-${words[w].word}.pdf`, function(err, res) {
-        if (err) throw err
-        console.log(res)
-      })
+      pdf
+        .create(html, options)
+        .toFile(`./tmp/pdf/${w}-${words[w].word}.pdf`, function(err, res) {
+          if (err) throw err
+          console.log(res)
+        })
     }
   } catch (error) {
     throw error
