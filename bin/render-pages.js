1a979dce47d873e0ed0fd0aec5d7625c9c086a32
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-03-30 17:28:26 -0400

    style updates

--- bin/render-pages.js
+++ bin/render-pages.js
@@ -7,17 +7,15 @@ const pdf = require("html-pdf")
 
 const run = async () => {
   try {
-    let words = await db.Word.find({}).sort("word")
+    let words = await db.Word.find({ section: { $ne: "Business" } }).sort("word")
     let options = {
-      format: "Letter",
-      paginationOffset: 1,
-      renderDelay: 1000,
-      footer: {
-        height: "28mm"
-      }
+      height: "10in", // allowed units: mm, cm, in, px
+      width: "8in", // allowed units: mm, cm, in, px      paginationOffset: 1,
+      renderDelay: 500,
     }
 
-    for (let w in words) { // TODO: render by section
+    for (let w in words) {
+      // TODO: render by section
       let path = await downloadAsync(words[w].word, w)
       let html = fs.readFileSync(path, "utf8")
 
